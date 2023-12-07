a4ac606ec170baa2b0b3fd88deb2438482a773fd
Jarien Reed <jireed@gmail.com>
2023-12-07 13:44:50 -0500

    5 - J. "Jack" Reed
    - added extrasite link to contact
    - made other changes to contact
    - fixed spacing on responsive contact
    - added rollover functionality to the contact page

--- js/contact.js
+++ js/contact.js
@@ -9,10 +9,25 @@ const address2 = document.getElementById('address2')
 
 const form =  document.getElementById('form')
 
+
+const submitButton = document.querySelector('button[type="submit"]');
+
+submitButton.addEventListener('mouseover', function () {
+    submitButton.style.backgroundColor = 'blue';
+    submitButton.style.color = 'white';
+});
+
+submitButton.addEventListener('mouseout', function () {
+    submitButton.style.backgroundColor = '';
+    submitButton.style.color = '';
+});
+
+
+/*
 form.addEventListener('submit', (e) =>{
 
     e.preventDefault()
 })
-
+*/
 
 /* there should be a message, after pressing submit, that thanks the user for submitting a message*/
