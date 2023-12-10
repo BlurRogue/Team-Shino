7e4108d4448dd2d4dc9090204b15d4885c5ccc29
Jarien Reed <jireed@gmail.com>
2023-12-09 22:26:12 -0500

    11 - J. "Jack" Reed
    - added achievement icons, changed some achievement descriptions
    - renamed and reorganized file structure
    - added documentation

--- js/dropdown.js
+++
@@ -1,7 +0,0 @@
-// JavaScript Document
-const Hamburger = document.querySelector(".hamburger")
-const dropDownMenu = document.querySelector(".dropdown-menu")
-
-Hamburger.onclick = function () {
-	dropDownMenu.classList.toggle("open")
-	const isOpen = dropDownMenu.classList.contains("open")
