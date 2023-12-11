8871d8459bf241509b4c2c49f4c6bb6695d7c632
makurashima <75916373+makurashima@users.noreply.github.com>
2023-12-10 22:30:16 -0500

    Update dropdown.js

--- JS/dropdown.js
+++ JS/dropdown.js
@@ -1,7 +1,25 @@
 // JavaScript Document
-const Hamburger = document.querySelector(".hamburger")
+const hamburger = document.querySelector(".hamburger")
 const dropDownMenu = document.querySelector(".dropdown-menu")
 
-Hamburger.onclick = function () {
-	dropDownMenu.classList.toggle("open")
-	const isOpen = dropDownMenu.classList.contains("open")
+
+if (hamburger && dropDownMenu) {
+    console.log("Script loaded");
+ 
+    hamburger.addEventListener("click", function () {
+       console.log("Hamburger clicked");
+       hamburger.classList.toggle("open");
+       dropDownMenu.classList.toggle("open");
+    });
+ 
+    // Close dropdown when clicking outside
+    document.addEventListener("click", function (event) {
+       if (!dropDownMenu.contains(event.target) && !hamburger.contains(event.target)) {
+          console.log("Clicked outside, closing dropdown");
+          hamburger.classList.remove("open");
+          dropDownMenu.classList.remove("open");
+       }
+    });
+ } else {
+    console.error("Hamburger or dropdown not found. Check your class names.");
+ }
